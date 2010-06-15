0758d11ab32cf76918de822ca9bd89f207f27cf9
Scott Beck <scottbeck@gmail.com>
2010-06-15 10:40:54 -0400

    Have Pod::Weaver::Section::Name use Class::Discover is no package can be found. This makes things play well with MXD

--- lib/Pod/Weaver/Section/Name.pm
+++ lib/Pod/Weaver/Section/Name.pm
@@ -34,10 +34,23 @@ sub weave_section {
 
   my $filename = $input->{filename} || 'file';
 
+  my $package;
+  if ($pkg_node) {
+    $package = $pkg_node->namespace;
+  }
+  else {
+    require Class::Discover;
+    my $classes = eval {
+      Class::Discover->discover_classes({
+        ppi_document => $ppi_document->clone
+      });
+    };
+    if ($classes) {
+     ($package) = keys %{ $classes->[0] };
+    }
+  }
   Carp::croak sprintf "couldn't find package declaration in %s", $filename
-    unless $pkg_node;
-
-  my $package = $pkg_node->namespace;
+    unless $package;
 
   my ($abstract)
     = $ppi_document->serialize =~ /^\s*#+\s*ABSTRACT:\s*(.+)$/m;
